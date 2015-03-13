a7d917b1c6caf353b024fd86258b3024cd7b8fbd
Alexander Harding <alexander.harding@software.dell.com>
2015-03-12 21:33:16 -0500

    more example stuff@

---
+++ script.js
@@ -0,0 +1,30 @@
+angular.module('app', ['smart-table', 'stRowDetail'])
+  .controller('mainCtrl', function($scope) {
+    var
+      nameList = ['Pierre', 'Pol', 'Jacques', 'Robert', 'Elisa'],
+      familyName = ['Dupont', 'Germain', 'Delcourt', 'bjip', 'Menez'];
+
+    function createRandomItem() {
+      var
+        firstName = nameList[Math.floor(Math.random() * 4)],
+        lastName = familyName[Math.floor(Math.random() * 4)],
+        age = Math.floor(Math.random() * 100),
+        email = firstName + lastName + '@whatever.com',
+        balance = Math.random() * 3000;
+
+      return {
+        firstName: firstName,
+        lastName: lastName,
+        age: age,
+        email: email,
+        balance: balance
+      };
+    }
+
+    $scope.itemsByPage=15;
+
+    $scope.rowCollection = [];
+    for (var j = 0; j < 200; j++) {
+      $scope.rowCollection.push(createRandomItem());
+    }
+  });
